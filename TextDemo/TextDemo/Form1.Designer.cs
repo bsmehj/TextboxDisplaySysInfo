50291d40378021abc1ebbb0d67d8467df66ecbf0
bsmehj <bsmehj@163.com>
2020-12-18 14:57:43 +0800

    添加实现文件winform

---
+++ TextDemo/TextDemo/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace TextDemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxTest = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timerDemo = new System.Windows.Forms.Timer(this.components);
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxTest
+            // 
+            this.textBoxTest.BackColor = System.Drawing.Color.DarkCyan;
+            this.textBoxTest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxTest.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxTest.ForeColor = System.Drawing.Color.White;
+            this.textBoxTest.Location = new System.Drawing.Point(12, 12);
+            this.textBoxTest.Multiline = true;
+            this.textBoxTest.Name = "textBoxTest";
+            this.textBoxTest.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxTest.Size = new System.Drawing.Size(332, 236);
+            this.textBoxTest.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(366, 49);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "启动";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timerDemo
+            // 
+            this.timerDemo.Interval = 500;
+            this.timerDemo.Tick += new System.EventHandler(this.timerDemo_Tick);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(366, 96);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "停止";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(366, 146);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "清空";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 260);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxTest);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTest;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timerDemo;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ TextDemo/TextDemo/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextDemo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        public void DispalyInfoMsg(string msg)
+        {
+            string err = string.Empty;
+            string str = string.Empty;
+            try
+            {
+                if (textBoxTest.InvokeRequired)
+                {
+                    this.BeginInvoke((Action)(() =>
+                    {
+                        str = string.Format("{0} {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), msg) +
+                            Environment.NewLine;
+                        textBoxTest.Text = textBoxTest.Text.Insert(0, str);
+                        textBoxTest.ScrollToCaret();
+                    }));
+                }
+                else
+                {
+                    str = string.Format("{0} {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), msg) +
+                        Environment.NewLine;
+                    textBoxTest.Text = textBoxTest.Text.Insert(0, str);
+                    textBoxTest.ScrollToCaret();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                err = "清空数据出现错误：" + ex.Message;
+                MessageBox.Show(msg);
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            timerDemo.Enabled = true;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timerDemo.Enabled = false;
+        }
+
+        /// <summary>
+        /// 追加文件
+        /// </summary>
+        /// <param name="Path">文件路径</param>
+        /// <param name="strings">内容</param>
+        public void FileAdd(string filepath, string strings)
+        {
+            FileStream fs = new FileStream(filepath, FileMode.OpenOrCreate|FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs); // 创建写bai入duzhi流
+            //sw.WriteLine(strings); // 写入Hello World
+            //sw.Close(); //关闭文件
+
+            //StreamWriter sw = File.AppendText(strings);
+            sw.Write(strings);
+            sw.Flush();
+            sw.Close();
+            sw.Dispose();
+            fs.Close();
+            fs.Dispose();
+        }
+
+        private void timerDemo_Tick(object sender, EventArgs e)
+        {
+            DispalyInfoMsg("ABC");
+
+            string target = Application.StartupPath + @"\SystemInfo\";
+            // 检查目标文件夹是否存在，不存在则创建
+            if (Directory.Exists(target) == false)
+            {
+                Directory.CreateDirectory(target);
+            }
+            string filePath = target + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+
+            // 数据回滚实现
+            // int lineNum = textBoxTest.Lines.Count();
+            int num = textBoxTest.Lines.Count();
+            if (num >= 11) 
+            {
+                int start = textBoxTest.GetFirstCharIndexFromLine(num - 2);
+                int end = textBoxTest.GetFirstCharIndexFromLine(num - 1);
+                textBoxTest.Select(start, end);
+                FileAdd(filePath, textBoxTest.SelectedText);
+                textBoxTest.SelectedText = "";
+
+                // timerDemo.Enabled = false;
+                // FileAdd(string filepath, string strings)
+            }
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxTest.Clear();
+        }
+    }
+}
